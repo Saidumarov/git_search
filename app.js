91e6b66d4d0e79eb9686052a697895db89cd8a55
Saidumarov <saidumarovjafarxon@gmail.com>
2023-05-08 16:21:26 +0500

    commit new

--- app.js
+++ app.js
@@ -5,21 +5,17 @@ let btn = document.querySelector("button")
 
 
 
-    form.addEventListener("click", (e) => {
-        e.preventDefault();
-        resform()
-
-    })
-
-
-   
-function ok() {
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  resform()
+btn.addEventListener("click" , () =>{
+  
   let ui = "";
-  fetch('https:api.github.com/users/'+ input.value )
-      .then(response => response.json())
-      .then(json => {
+  fetch('https:api.github.com/users/' + input.value)
+    .then(response => response.json())
+    .then(json => {
 
-          ui += `
+      ui += `
           <div class="img">
           <img src=${json.avatar_url} alt="" />
         </div>
@@ -76,52 +72,59 @@ function ok() {
         </div>
               `
 
-          result.innerHTML = ui
-      })
+      result.innerHTML = ui
+    })
 
-  
-}
+
+})
+})
+
+
+
+
+
+
 const resform = () => {
-    input.value = ""
+  input.value = ""
 }
 
 function ok1() {
 
-    if (document.querySelector(".oy").style.left = "-52.7%") {
-        document.querySelector("body").style.backgroundColor = "white"
-        document.querySelector(".natija").style.backgroundColor = "#F7FAFC"
-        document.querySelector(".mom").style.backgroundColor = "rgb(210, 201, 201)"
-        document.querySelector(".mn").style.color = String("rgb(43, 42, 90)")
-        document.querySelector("input").style.backgroundColor = "#718096"
-        document.querySelector("button").style.backgroundColor = "#718096"
-        document.querySelector("button").style.color = "white"
-        document.querySelector(".natija").style.boxShadow = "2px 2px 5px 5px rgb(210, 201, 201)";
-        document.querySelector(".mom").style.borderColor = "#718096"
-        document.querySelector(".flowers").style.backgroundColor = "#4B6A9B"
-        document.querySelector(".result").style.color = "black"
-        document.querySelector(".flowers").style.color = "white"
+  if (document.querySelector(".oy").style.left = "-52.7%") {
+    document.querySelector("body").style.backgroundColor = "white"
+    document.querySelector(".natija").style.backgroundColor = "#F7FAFC"
+    document.querySelector(".mom").style.backgroundColor = "rgb(210, 201, 201)"
+    document.querySelector(".mn").style.color = String("rgb(43, 42, 90)")
+    document.querySelector("input").style.backgroundColor = "#718096"
+    document.querySelector("button").style.backgroundColor = "#718096"
+    document.querySelector("button").style.color = "white"
+    document.querySelector(".natija").style.boxShadow = "2px 2px 5px 5px rgb(210, 201, 201)";
+    document.querySelector(".mom").style.borderColor = "#718096"
+    document.querySelector(".flowers").style.backgroundColor = "#4B6A9B"
+    document.querySelector(".result").style.color = "black"
+    document.querySelector(".flowers").style.color = "white"
 
 
 
 
-    }
+  }
 }
 function ok2() {
-    if (document.querySelector(".oy").style.left = "0%") {
-        document.querySelector("body").style.backgroundColor = String(" #171923")
-        document.querySelector(".natija").style.backgroundColor = " #1A202C"
-        document.querySelector(".mom").style.backgroundColor = ""
-        document.querySelector(".mn").style.color = ""
-        document.querySelector("button").style.backgroundColor = ""
-        document.querySelector("input").style.backgroundColor = ""
-        document.querySelector("button").style.color = ""
-        document.querySelector("input").style.color = "white"
-        document.querySelector(".natija").style.boxShadow = "";
-        document.querySelector(".mom").style.borderColor = ""
-        document.querySelector(".flowers").style.backgroundColor = ""
-        document.querySelector(".result").style.color = ""
+  if (document.querySelector(".oy").style.left = "0%") {
+    document.querySelector("body").style.backgroundColor = String(" #171923")
+    document.querySelector(".natija").style.backgroundColor = " #1A202C"
+    document.querySelector(".mom").style.backgroundColor = ""
+    document.querySelector(".mn").style.color = ""
+    document.querySelector("button").style.backgroundColor = ""
+    document.querySelector("input").style.backgroundColor = ""
+    document.querySelector("button").style.color = ""
+    document.querySelector("input").style.color = "white"
+    document.querySelector(".natija").style.boxShadow = "";
+    document.querySelector(".mom").style.borderColor = ""
+    document.querySelector(".flowers").style.backgroundColor = ""
+    document.querySelector(".result").style.color = ""
 
 
 
-    }
+  }
 }
